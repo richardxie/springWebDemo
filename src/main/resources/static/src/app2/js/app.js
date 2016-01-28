06e33f0678adf54505cce11d35567a943c49448f
richard.xieq <xieqiang@shanlinjinrong.com>
2016-01-28 16:21:15 +0800

    add rxjs demo
    add rxjs demo

--- src/main/resources/static/src/app2/js/app.js
+++ src/main/resources/static/src/app2/js/app.js
@@ -14,46 +14,62 @@ exports.getTokens = function() {
 
 
 angular.module('rxApp', [ 'rx', 'rxApp.directive'])
-		.config(["$httpProvider", '$locationProvider', function($httpProvider, $locationProvider) {
+	.config(["$httpProvider", '$locationProvider', function($httpProvider, $locationProvider) {
 			$httpProvider.defaults.headers.common[_csrf_header] = _csrf_token;
 			$locationProvider.html5Mode(true);
-		}])
-		.controller('RxCtrl', ["$scope", "$http", "rx", function($scope, $http, rx) {
+	}])
+	.controller('RxCtrl', ["$scope", "$http", "rx", function($scope, $http, rx) {
 
-			function searchCust(term) {
-				var deferred = $http({
-					url : "/cust/searchCust",
-					method : "post",
-					data : {
-						action : "namesearch",
-						search : term,
-						format : "json"
+		function searchCust(term) {
+			var deferred = $http({
+				url : "/cust/searchCust",
+				method : "post",
+				data : {
+					action : "namesearch",
+					search : term,
+					format : "json"
+				}
+			});
+
+			return rx.Observable
+				.fromPromise(deferred)
+				.retry(2) // Retry 10 times then give up
+				.map(
+					function(response) {
+						console.log(response);
+						return response.data;
 					}
-				});
+				);
+		}
 
-				return rx.Observable.fromPromise(deferred).map(
-						function(response) {
-							console.log(response);
-							return response.data;
-					});
-			}
+		$scope.search = '';
+		$scope.results = [];
 
-			$scope.search = '';
-			$scope.results = [];
+		/*
+		 * The following code deals with: Creates a "submit"
+		* function which is an observable sequence instead of
+		* just a function.
+		*/
+		$scope.$createObservableFunction('submit')
+			.debounce(300)
+			.map(function(term) {
+				console.log('submit clicked!' + term)
+				return term;
+			})
+			.flatMapLatest(searchCust)
+			.subscribe(
+				function(results) {
+					$scope.results = results;
+					$scope.$apply();
+					console.log($scope.results);
+				}
+			);
 
-			/*
-			 * The following code deals with: Creates a "submit"
-			* function which is an observable sequence instead of
-			* just a function.
-			*/
-			$scope.$createObservableFunction('submit')
-				.map(function(term) {
-					console.log('submit clicked!')
-					return term;
-				})
-				.flatMapLatest(searchCust).subscribe(
-					function(results) {
-						$scope.results = results;
-						console.log($scope.results);
-				});
-		}]);
+		$scope.$toObservable('search')
+			.debounce(300)
+			.map(function(data) { console.log(data); return data.newValue; })
+			.distinctUntilChanged()
+			.flatMapLatest(searchCust)
+			.subscribe(function(val){ $scope.results = val; $scope.$apply();});
+		}
+	]);
